bf4e62cd4931d3a715653454144cbfd8d1164a03
Enrique Sanchez <kikin1601@hotmail.com>
2016-01-26 15:28:19 -0500

    personalizacion de colores ok

--- js/main.js
+++ js/main.js
@@ -6,6 +6,32 @@ $(document).ready(function() {
     
     $( ".infoAlbum" ).css( 'width',$( ".cuerpo" ).width()-290 );
     
+    var img = document.createElement('img');
+    img.setAttribute('src', 'img/250x250.jpg');
+
+    img.addEventListener('load', function() {
+        console.log("inicia la carga");
+        var vibrant = new Vibrant(img);
+        var swatches = vibrant.swatches()
+        for (var swatch in swatches)
+            if (swatches.hasOwnProperty(swatch) && swatches[swatch])
+                console.log(swatch, swatches[swatch].getHex())
+
+                /*
+     * Results into:
+     * Vibrant #7a4426
+     * Muted #7b9eae
+     * DarkVibrant #348945
+     * DarkMuted #141414
+     * LightVibrant #f3ccb4
+     */
+                console.log(swatches['Muted'].getHex());
+        $(".cabAlbum").css('background',swatches['Muted'].getHex())
+        $(".cabAlbum").css('color',swatches['DarkVibrant'].getHex())
+        $(".cabAlbum h2").css('color',swatches['DarkVibrant'].getHex())
+        $(".cabAlbum .detalles").css('color',swatches['DarkVibrant'].getHex())
+//        $(".cabAlbum").css("background-image","img/250x250.jpg");
+                });
     
 
 });
