669d1366e736f615fabc0e50907bb81a100f8acb
Enrique Sanchez <kikin1601@hotmail.com>
2016-01-27 19:01:11 -0500

    Cambio de textos en la cabecera

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
+    var texts=["Rude","No way no","Am i wrong","Carry on","Come Nights","We are Young (feat Janelle Monáe)","Counting star","Good life","Break a sweat","Rude","Love me hander","Animals","Un día sin sexo","Cosa imposibles","Mil horas"];
+    var artistas=["Magic!","Magic!","Nico & Vinz","Fun","Fun","Fun","One Republic","One Republic","Becky G","Magic!","Ariana Grande","Marron 5","Mar de copas","Gustavo Cerati","Los abuelos de la nada"];
     
     $( ".wrapper" ).css( 'height',$( window ).height() );
-    $( ".cuerpo" ).css( 'width',$( window ).width()-220 );
+    $( ".cuerpo" ).css( 'width',$( window ).width()-203 );
     
     
     $( ".infoAlbum" ).css( 'width',$( ".cuerpo" ).width()-290 );
     
-    var img = document.createElement('img');
+    /*var img = document.createElement('img');
     img.setAttribute('src', 'img/250x250.jpg');
 
     img.addEventListener('load', function() {
@@ -17,23 +19,73 @@ $(document).ready(function() {
             if (swatches.hasOwnProperty(swatch) && swatches[swatch])
                 console.log(swatch, swatches[swatch].getHex())
 
-                /*
+                
      * Results into:
      * Vibrant #7a4426
      * Muted #7b9eae
      * DarkVibrant #348945
      * DarkMuted #141414
      * LightVibrant #f3ccb4
-     */
+     
                 console.log(swatches['Muted'].getHex());
-        $(".cabAlbum").css('background',swatches['Vibrant'].getHex())
-        $(".cabAlbum").css('color',swatches['Muted'].getHex())
-        $(".cabAlbum h2").css('color',swatches['DarkMuted'].getHex())
-        $(".cabAlbum .detalles").css('color',swatches['DarkMuted'].getHex())
-//        $(".cabAlbum").css("background-image","img/250x250.jpg");
-                });
+        $(".cabAlbum").css('background',swatches['DarkVibrant'].getHex())
+        $(".cabAlbum").css('color',swatches['LightVibrant'].getHex())
+        $(".cabAlbum h2").css('color',swatches['LightVibrant'].getHex())
+        $(".cabAlbum .detalles").css('color',swatches['LightVibrant'].getHex())
+    });*/
     
-
+    function llenaTexto(idt){
+        $(".cabAlbum h1").html(texts[idt-1]);
+        $(".cabAlbum h2").html(artistas[idt-1]);
+        console.log("llena texto: "+idt);
+        TweenMax.to($(".cabAlbum h1"), 0.3, { alpha:1});
+        TweenMax.to($(".cabAlbum h2"), 0.3, { alpha:1});
+    }
+    
+    function Foto(contenido){
+        var img = document.createElement('img');
+        img.setAttribute('src', contenido);
+        img.addEventListener('load', function() {
+            var vibrant = new Vibrant(img);
+            var swatches = vibrant.swatches();
+            for (var swatch in swatches){
+                if (swatches.hasOwnProperty(swatch) && swatches[swatch]){
+//                    console.log(swatch, swatches[swatch].getHex())
+                }
+            }
+//            console.log(swatches['Vibrant'].getHex());
+            TweenMax.to($(".cabAlbum"), 0.5, {backgroundColor:swatches['DarkVibrant'].getHex()});
+            
+//            $(".cabAlbum").css('background',swatches['DarkVibrant'].getHex());
+            $(".cabAlbum").css('color',swatches['LightVibrant'].getHex());
+            $(".cabAlbum h2").css('color',swatches['LightVibrant'].getHex());
+            $(".cabAlbum .detalles").css('color',swatches['LightVibrant'].getHex());
+        });
+    }
+    
+    
+    
+    var foto = new Foto($(".conteFoto>img").attr("src"));
+    for(var i=1;i<=15;i++){
+        
+        //console.log("listado: " + $(".table>tbody>#fila"+i+">td>.namePlay").attr('id'));
+        
+        $(".table>tbody>#fila"+i+">td>.namePlay").click(function(){
+            var IDs = $(this).attr('id');
+            console.log(IDs);
+            TweenMax.to($(".conteFoto>img"), 0, { alpha:0});
+            TweenMax.to($(".cabAlbum h1"), 0.3, { alpha:0, onComplete:llenaTexto, onCompleteParams:[IDs]});
+            TweenMax.to($(".cabAlbum h2"), 0.3, { alpha:0});
+            
+            $(".conteFoto>img").attr("src","img/250x250_"+IDs+".jpg");
+            TweenMax.to($(".conteFoto>img"), 0.5, { alpha:1,delay:0.2});
+//            TweenMax.from($(".conteFoto>img"), 0.5, { scaleX:0.8,scaleY:0.8,delay:0.2});
+            
+            var foto = new Foto($(".conteFoto>img").attr("src"));
+            
+        });
+    }
+    
 });
 $( window ).resize(function() {
     $( ".wrapper" ).css( 'height',$( window ).height() );
--- js/vibrant.js
+++ js/vibrant.js
@@ -1,23 +1,5 @@
 $(document).ready(function() {
     
-/*        var img = document.createElement('img');
-        img.setAttribute('src', $("#foto_0>img").attr("src"));
-        img.addEventListener('load', function() {
-            var vibrant = new Vibrant(img);
-            var swatches = vibrant.swatches();
-            for (var swatch in swatches){
-                if (swatches.hasOwnProperty(swatch) && swatches[swatch]){console.log(swatch, swatches[swatch].getHex())}
-            }
-            $("#foto_0").css('background',swatches['Vibrant'].getHex());
-            $("#foto_0>.colores>ul>.Vibrant").css('background',swatches['Vibrant'].getHex());
-            $("#foto_0>.colores>ul>.Muted").css('background',swatches['Muted'].getHex());
-            $("#foto_0>.colores>ul>.DarkVibrant").css('background',swatches['DarkVibrant'].getHex());
-            $("#foto_0>.colores>ul>.DarkMuted").css('background',swatches['DarkMuted'].getHex());
-            $("#foto_0>.colores>ul>.LightVibrant").css('background',swatches['LightVibrant'].getHex());
-        });*/
-        
-    
-    
     function Foto(contenido,target){
         var img = document.createElement('img');
         img.setAttribute('src', contenido);
@@ -28,7 +10,8 @@ $(document).ready(function() {
                 if (swatches.hasOwnProperty(swatch) && swatches[swatch]){console.log(swatch, swatches[swatch].getHex())}
             }
             console.log(swatches['Vibrant'].getHex());
-            $(target).css('background',swatches['Vibrant'].getHex());
+            $(target).css('background',swatches['DarkVibrant'].getHex());
+            $(target+">.colores>ul>li>p").css('color',swatches['LightVibrant'].getHex());
             $(target+">.colores>ul>.Vibrant").css('background',swatches['Vibrant'].getHex());
             $(target+">.colores>ul>.Muted").css('background',swatches['Muted'].getHex());
             $(target+">.colores>ul>.DarkVibrant").css('background',swatches['DarkVibrant'].getHex());
@@ -37,10 +20,7 @@ $(document).ready(function() {
         });
     }
     
-    for(var i=0;i<=8;i++){
+    for(var i=0;i<=13;i++){
         var foto = new Foto($("#foto_"+i+">img").attr("src"),"#foto_"+i);
     }
-    /*var foto0 = new Foto($("#foto_0>img").attr("src"),"#foto_0");
-    var foto1 = new Foto($("#foto_1>img").attr("src"),"#foto_1");
-    var foto2 = new Foto($("#foto_2>img").attr("src"),"#foto_2");*/
 });
